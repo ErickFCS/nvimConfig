e453e212c5b1185b51f16f203b2ef0ab698f5a0e
Erick Fernando Carvalho Sanchez <carvalhosanchezerickfernando@gmail.com>
2025-04-09 18:25:27 -0300

    Simple

---
+++ lua/config/options.lua
@@ -0,0 +1,16 @@
+vim.o.shiftwidth = 4
+vim.o.foldmethod = "indent"
+vim.o.cursorline = true
+vim.o.cursorcolumn = true
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.statusline = "  %n | %f %r %m %{&filetype} | %= | %l:%c %p%% " 
+vim.cmd('hi Normal guibg=none')
+vim.cmd('hi NonText guibg=none')
+vim.cmd('hi Normal guibg=none')
+vim.cmd('hi NormalNC guibg=none')
+vim.cmd('hi NvimTreeNormal guibg=none')
+vim.cmd('hi VertSplit guibg=none')
+vim.cmd('hi SignColumn guibg=none')
+vim.cmd('hi EndOfBuffer guibg=none')
+vim.cmd('hi StatusLine guibg=none')
