b97e23a283d1ba4bff2da895024bf6478ac0af8b
Erick Fernando Carvalho Sanchez <carvalhosanchezerickfernando@gmail.com>
2026-01-26 17:44:07 -0300

    Mini plugins implemented

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,1 +1,14 @@
-return { 'echasnovski/mini.nvim', version = '*' }
+return {
+  {
+    "echasnovski/mini.nvim",
+    version = "*", -- Use the latest development version
+    config = function()
+      -- Improve text object allowing better seelct inside
+      require('mini.ai').setup()
+      -- easy change of surrounding quotes
+      require('mini.surround').setup()
+      -- Auto insert closeing pair
+      require('mini.pairs').setup()
+    end,
+  },
+}
